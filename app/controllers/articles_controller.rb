949b7d4d4d2fe58debb53832d81910f314bbbe4c
Yato Aki <yatoakii@gmail.com>
2022-05-10 10:51:51 +0630

    Fixed html format, added http auth where required

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "secret", only: [:destroy,:edit,:update]
 
   def index
     @articles = Article.all
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+articles_list =[
+  {title: "What a nice rainy day", body: "I love rains. Do you feel the same too?", status: "public"},
+  {title: "I see trees of gree,", body: "red roes too. I see them bloom for me and for you", status: "public"}]
+
+Article.create!(articles_list)
